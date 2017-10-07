c10a7c1fb83970e314260ccba363d377d6cac975
Poulpinou <donovan.persent@gmail.com>
2017-10-07 18:48:37 +0200

    synch with Remi

--- Assets/Scripts/WorldObjects/BuildingElements/BuildingElement.cs
+++ Assets/Scripts/WorldObjects/BuildingElements/BuildingElement.cs
@@ -5,6 +5,22 @@ using bloup;
 
 public class BuildingElement : WorldObject {
     protected bool builded;
+    int value;
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public bool isInRange()
+    {
+        /*int width = Screen.width;
+        int quartWidth = width / 100 * 25;
+        Debug.Log("25% : " + quartWidth);
+        int troisquartWidth = width / 100 * 75;
+        Debug.Log("75% : " + troisquartWidth);*/
+        return transform.position.x >= -10 && transform.position.x <= 10;
+    }
 
     public bool isBuilt()
     {
